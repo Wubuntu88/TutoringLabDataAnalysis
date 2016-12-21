4dc49341c890bcd3c258babdfdecf1355e699eba
William Gillespie <wgilles1@emich.edu>
2016-12-20 23:54:51 -0500

    Now the generage aggregate script will generate the entire table correctly (to my knowledge).

---
+++ data_processing/generate_aggregate_file.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import os
+import string
+
+"""
+This script will go through the data in the RawData folder, process it,
+and print out the processed data.  To store the output in a file,
+redirect IO to a file.  For example, you could type this on the command line:
+./generate_aggregate_file.py > aggregate_data.tsv
+"""
+
+def process_line(line, date_prefix):
+    components = line.split('\t')
+    components[0] = process_time_field(time=components[0], date_prefix=date_prefix)  # add date to time field
+    components[3] = process_language_field(language_field=components[3])
+    components[4] = process_name(components[4])
+    return "\t".join(components)
+
+
+def process_time_field(time, date_prefix):
+    return date_prefix + ' ' + time
+
+
+def process_language_field(language_field):
+    if language_field == '':
+        return "NA"
+    elif language_field == 'LC-3' or language_field == 'LC3':
+        return 'LC3'
+
+    if len(language_field) >= 2:
+        index_of_comma = language_field.find(',')  # see if someone put a comma
+        if index_of_comma != -1:
+            language_field = language_field[:index_of_comma]  # if so, throw away whats after it
+        return language_field[0].upper() + language_field[1:].lower()
+    else:
+        return language_field
+
+
+def process_name(name):
+    return name[0].upper() + name[1:].lower()
+
+
+records_to_write = []
+relative_path_to_data_directory = '../RawData'
+file_names = os.listdir(relative_path_to_data_directory)
+
+for f_name in file_names:
+    comps = f_name.split()
+    the_date_str = string.replace(comps[-1], '.tsv', '')
+    path_to_the_file = relative_path_to_data_directory + '/' + f_name
+    the_file = open(path_to_the_file, 'r')
+    header = the_file.readline()  # ignore header
+    for line in the_file:
+        line = line.rstrip('\n')
+        if line[-1] == "\n":
+            print("------READ NEWLINE------")
+        new_line = process_line(line, the_date_str)
+        print(new_line)
+    the_file.close()
+
